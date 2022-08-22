94165e785a20ab18078e690eaf5ab04cb7ef902b
PhiloCwh <105807963+PhiloCwh@users.noreply.github.com>
2022-08-22 17:44:01 +0800

    Update functioncall_other_Contract.sol

--- functioncall_other_Contract.sol
+++ functioncall_other_Contract.sol
@@ -2,7 +2,10 @@
 pragma solidity ^0.8.0;
 
 contract B {
-
+/**
+*@dev (address).call(abi.encodeWithSignature("function(param)",realparam)
+*@return (bool,bytes)
+*/
     function callMintNFT(address _contract, address to) public payable returns(bool){
         // A's storage is set, B is not modified.
         (bool success, bytes memory data) = _contract.call(
